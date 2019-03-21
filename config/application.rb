54ec70c55aa44145a1fc7f014ed6acc04f306e1a
Takahiro Nakamura <hikusema189@keio.jp>
2019-03-21 19:47:46 +0900

    add spec

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,18 @@ module EcSample
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.test_framework :rspec,
+        fixtures: true,
+        view_spacs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
   end
 end
